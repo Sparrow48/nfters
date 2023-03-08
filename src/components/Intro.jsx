7cb30b725b69d1f54e4288cc809e3f54ef7134fc
Sparrow48 <abunasib48@gmail.com>
2023-03-08 21:22:15 +0600

    edit intro section.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,25 +1,40 @@
 import React from 'react'
 import nfts1 from './../assets/NFTS1.png'
 import nfts2 from './../assets/NFTS2.png'
+import Profile from './../assets/man.png'
 
 const Intro = () => {
     return (
         <div>
             <div className='flex flex-col px-4 mx-auto my-10 lg:px-32 lg:grid lg:grid-cols-2 lg:gap-20 '>
-                <div className='w-1/2 pb-24 ml-32 lg:pb-0 lg:order-2'>
-                    <div className='container relative bg-white w-52 h-72'>
+                <div className='w-3/4 pb-24 ml-48 lg:pb-0 lg:order-2'>
+                    <div className='container relative bg-white w-72 h-72'>
                         <img src={nfts1} alt="" className='absolute top-0 w-full h-72 right-30 rounded-xl' />
                         <img src={nfts2} alt="" className='absolute top-0 w-full h-full right-10 rounded-xl' />
                         <img src={nfts1} alt="" className='absolute top-0 w-full h-full right-20 rounded-xl' />
-                        <div className='absolute bottom-0 bg-blue-300'>
+
+                        <div className='absolute p-3 mx-5 text-white rounded-lg top-5 -left-16'>
                             <div className='flex gap-5'>
 
                                 <div className='flex flex-col'>
-                                    <h5>Current Bit</h5>
+                                    <h5 className='text-xl font-semibold'>Abstr Gradient NFT </h5>
+                                    <div className='flex gap-2'>
+                                        <img src={Profile} height='40' width='25' className='rounded-full ' alt="" />
+                                        <h5>ArKhan17</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='absolute p-3 mx-5 text-white bg-blue-300 rounded-lg bottom-5 -left-16'>
+                            <div className='flex gap-5'>
+
+                                <div className='flex flex-col'>
+                                    <h5 className='text-xs '>Current Bit</h5>
                                     <h5>0.25 ETH</h5>
                                 </div>
                                 <div className='flex flex-col'>
-                                    <h5>Ends in </h5>
+                                    <h5 className='text-xs' >Ends in </h5>
                                     <h5>12h 43m 42s</h5>
                                 </div>
                             </div>
